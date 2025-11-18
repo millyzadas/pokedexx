072f7fad0b8d8dc34658558fdee4f199a89cd8c7
millyzadas <emillycavalcante987@gmail.com>
2025-11-18 11:08:26 -0300

    Update cadastrar.py

--- cadastrar.py
+++ cadastrar.py
@@ -7,8 +7,8 @@ def app():
         nome = st.text_input("Digite seu nome")
         cidade = st.text_input("Digite sua cidade")
         imagem = st.file_uploader("Foto de perfil")
-        id = st.text_input("Crie seu ID de treinador")
-        confirm_id = st.text_input("Confirme seu ID de treinador")
+        id = st.text_input("Crie seu código de treinador")
+        confirm_id = st.text_input("Confirme seu código de treinador")
         button = st.form_submit_button('Cadastrar')
     
     st.write('Já tem uma conta?')
@@ -16,13 +16,13 @@ def app():
         st.session_state.pagina = "perfil"
         
     if button:
-        if confirm_id != id:
+        if confirm_código != código:
             st.warning('Os IDs não se coincidem')
         else:
-            treinadorExiste = functions.verificar_treinador(id)
+            treinadorExiste = functions.verificar_treinador(código)
             if treinadorExiste == False:
-                functions.inserir_treinador(nome, cidade, imagem, id)
+                functions.inserir_treinador(nome, cidade, imagem, código)
                 st.success('Treinador adicionado')
                 st.balloons()
             else:
-                st.warning(f"O treinador com o id: {id} já existe!")
+                st.warning(f"O treinador com o código: {código} já existe!")
