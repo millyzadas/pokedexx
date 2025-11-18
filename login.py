dbaf2c8ba0d7a616ca4e47ce2e476e226ceefc7f
millyzadas <emillycavalcante987@gmail.com>
2025-11-18 10:42:45 -0300

    Create login.py

---
+++ login.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def app():
+    st.header('Entrar')
+    with st.form('button'):
+        nome = st.text_input("Digite seu nome")
+        cidade = st.text_input("Digite sua cidade")
+        id = st.text_input("Digite seu ID de treinador")
+        button = st.form_submit_button('Entrar')
+
+    st.write("Não tem conta?")
+    if st.button("Cadastre-se"):
+        st.session_state.pagina = "cadastrar"
