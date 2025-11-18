37af25b8d6ba6de57b26941a26eae8567d13a2b4
millyzadas <emillycavalcante987@gmail.com>
2025-11-18 10:43:34 -0300

    Create pokedex.py

---
+++ pokedex.py
@@ -0,0 +1,28 @@
+
+import streamlit as st
+import functions
+
+def app():
+    st.title("Captura de Pokémon")
+    with st.form('button'):
+        pokemon = st.text_input("Digite o nome do Pokémon capturado").lower()
+        id = st.text_input("Digite o id do treinador que o capturou")
+        imagem = st.file_uploader("Envie uma imagem do Pokémon", type=["png", "jpg", "jpeg"])
+        button = st.form_submit_button('Capturar!')
+
+    if button:
+        existe = functions.verificar_treinador(id)
+        if existe == False:
+            st.warning("O treinador não existe!")
+        else:
+            functions.adicionar_pokemon(pokemon, id, imagem)
+            st.success("Pokémon capturado")
+            st.balloons()
+            
+    
+    st.header('Pokémons encontrados:')
+    functions.mostrar_pokemons()
+
+  
+
+
